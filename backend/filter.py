fecaeeaade1ccd1a21e36a267e4cd3699d908eff
Bram <bnutt@macalester.edu>
2024-02-23 18:39:49 +0100

    Dataset push

---
+++ backend/filter.py
@@ -0,0 +1,14 @@
+from profanity import predict, predict_prob
+class commentFilter:
+    # This class filters comments
+
+    def filterComment(datajson_file):
+        predict()
+        
+
+    filterComment("backend/datajson/1d00ea97a1d2d597ec4bf8c69c01b032462c0bad_1.json")
+
+
+
+
+
